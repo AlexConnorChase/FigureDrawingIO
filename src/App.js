9eaa83b6b373229f4f11a5c1484a084f0ce7baf6
Alex Chase <alex.chase@ufl.edu>
2024-10-01 17:44:55 -0400

    Adding initial webpage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <AppBody />
     </div>
+  );
+}
+
+function AppHeader() {
+  return (
+    <header className="App-header">
+      <p>FigureDrawing.io</p>
+    </header>
+  );
+}
+
+function AppBody() {
+  return (
+    <main className="App-body">
+      <AppTable />
+    </main>
+  );
+}
+
+function AppTable() {
+  return (
+    <table className="Content-table">
+      <tbody>
+        <tr>
+          <td>
+            <div className="App-description">
+              <p>
+                This app uses the tumblr API to find images for timed sketching
+                practices. Insert the tags you want to pick from, more filtering
+                options may be added in the future.
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className="Search-bar">
+              <span className="SB-span">
+                <form>
+                  <input
+                    type="text"
+                    id="tags"
+                    name="tags"
+                    placeholder="Input Tags Here"
+                  />
+                  <input type="submit" value="Search" />
+                </form>
+              </span>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className="Image-Display-Back">
+              <div className="Image-display"></div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
